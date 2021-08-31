c129f609827aae94f5a732cec1152360163c0a8f
itzdroyon <73495823+itzdroyon@users.noreply.github.com>
2021-08-31 06:41:51 +0200

    Add files via upload

--- codes/blips.lua
+++ codes/blips.lua
@@ -1,1 +1,21 @@
+local blips = {
+    -- Example {title="", colour=, id=, x=, y=, z=},
 
+     {title="Bahama Mamas Club", colour=3, id=93, x = -1388.16, y = -591.28, z = 30.32},
+     --{title="Marlowe Vineyard", colour=3, id=499, x = -1873.58, y = 2023.76, z = 138.83}
+  }
+      
+Citizen.CreateThread(function()
+
+    for _, info in pairs(blips) do
+      info.blip = AddBlipForCoord(info.x, info.y, info.z)
+      SetBlipSprite(info.blip, info.id)
+      SetBlipDisplay(info.blip, 4)
+      SetBlipScale(info.blip, 1.0)
+      SetBlipColour(info.blip, info.colour)
+      SetBlipAsShortRange(info.blip, true)
+	  BeginTextCommandSetBlipName("STRING")
+      AddTextComponentString(info.title)
+      EndTextCommandSetBlipName(info.blip)
+    end
+end)
